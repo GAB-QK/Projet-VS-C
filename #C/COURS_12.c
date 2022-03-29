c8a91cc8e22bac51ab923100f5f665d06db9dd8e
GAB-QK <Gabriel.qaddaha@ecole-hexagone.com>
2022-03-29 11:03:38 +0200

    save

--- #C/COURS_12.c
+++ #C/COURS_12.c
@@ -2,47 +2,92 @@
 #include <stdlib.h>
 #include <string.h>
 
-struct contact {
-  char* firstname;
-  char* lastname;
+struct contact{
+
+  char *firstname;
+  char *lastname;
   unsigned int age;
-  char* job;
-  unsigned int id ;
+  char *job;
+  struct contact *suivant;
 };
 
-void contact_print( struct contact contact ) {
-  printf("%s ", contact.firstname);
-  printf("%s a ", contact.lastname);
-  printf("%u ans et travail en tant que ", contact.age);
-  printf("%s", contact.job);
-  printf("%u\n",contact.id);
-}
+struct Liste{
+  struct contact *premier;
+};
 
-struct contact contact_create( char* firstname,  char* lastname, unsigned int age, char* job, unsigned int id) {
-  struct contact contact;
+struct Liste *initialisation()
+{
+    struct Liste *liste = malloc(sizeof(*liste));
+    struct contact *contact = malloc(sizeof(*contact));
 
-  contact.firstname = malloc( strlen(firstname) * sizeof(char) );
-  strcpy( contact.firstname, firstname );
-  contact.lastname = malloc(  strlen(lastname) * sizeof(char) );
-  strcpy( contact.lastname, lastname );
-  contact.job = malloc( strlen(job) * sizeof(char) );
-  strcpy( contact.job, job );
-  contact.age = age;
-
-  return contact;
-}
-
-int main(){
-    printf("combien dentrée dans la liste ?");
-    scanf("%u",counter );
-
-    for (int counter = 0; i < count; i++)
+    if (liste == NULL || contact == NULL)
     {
-
+        exit(EXIT_FAILURE);
     }
-    
 
-    contact_print( Bob );
+    contact->firstname = "gab";
+    contact->lastname = "le plus beau";
+    contact->age = 22;
+    contact->job = "riche";
+
+    contact->suivant = NULL;
+    liste->premier = contact; // premier > suivant > NULL 
+
+    return liste;
+}
+
+void insertion(struct Liste *liste, char* N_firstname,  char* N_lastname, unsigned int N_age, char* N_job)
+{
+    /* Création du nouvel élément */
+    struct contact *nouveau = malloc(sizeof(*nouveau));
+    if (liste == NULL || nouveau == NULL)
+    {
+        exit(EXIT_FAILURE);
+    }
+
+    nouveau->firstname = N_firstname;
+    nouveau->lastname = N_lastname;
+    nouveau->age = N_age;
+    nouveau->job = N_job;
+
+    /* Insertion de l'élément au début de la liste */
+    nouveau->suivant = liste->premier;
+    liste->premier = nouveau;
+}
+
+void afficherListe(struct Liste *liste)
+{
+    if (liste == NULL)
+    {
+        exit(EXIT_FAILURE);
+    }
+
+    struct contact *actuel = liste->premier;
+
+    while (actuel != NULL)
+    {
+        printf("%s ", actuel->firstname);
+        printf("/");
+        printf("%s ", actuel->lastname);
+        printf("/");
+        printf("%d ", actuel->age);
+        printf("/");
+        printf("%s -> ", actuel->job);
+        printf("\n");
+        actuel = actuel->suivant;
+    }
+    printf("NULL\n");
+}
+
+int main()
+{
+    struct Liste *maListe = initialisation();
+
+    insertion(maListe, "thomas", "G",19,"etudiant");
+    insertion(maListe, "aurelien", "IOH",12,"etudiant");
+    insertion(maListe, "alex", "erf",56,"etudiant");
+
+    afficherListe(maListe);
 
     return 0;
 }
